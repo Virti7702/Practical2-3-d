1dd7df639f4252ffe837e29af6876209f4de7d5c
Virti7702 <97676077+Virti7702@users.noreply.github.com>
2022-01-14 15:15:41 +0530

    Add files via upload

---
+++ pratical2_3_d.py
@@ -0,0 +1,10 @@
+'''
+Name - Virti Shah
+ID - 20CE135
+Aim - A python program to find max and min of the given value.
+Github link - https://github.com/Virti7702/Practical2-3-d.git
+
+'''
+s={1,2,3,4,5}
+print(max(s))
+print(min(s))
